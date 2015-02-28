c93d2237789ecd8557d258f5831bcfb0c29fbca2
Andrew Hart <andrew@pogoseat.com>
2015-02-28 09:51:13 -0800

    Factor existing content into 2012 directory

---
+++ 2012/public/javascripts/afh.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * 
+ * @author Andrew F. Hart (afh@andrewfhart.com)
+ **/
+ 
+var A = (function ($) {
+    
+    
+    /**
+     * The Public API for this object.
+     *
+     */
+    return {   
+    
+    };
+}(jQuery));
+
+
+
+$(document).ready( function () {
+
+});
